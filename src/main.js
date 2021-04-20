232476af4799d3e864490b1d96b105ece59cb1cb
jgpicos <jpicos@ucsc.edu>
2021-04-19 21:05:43 -0700

    Main Comment added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
+/* 
+Jesus Picos, Rocket Patrol Mods, April 19 2021, took ~10
+
+Point Break down: 
+    5:
+        -New Scrolling Background
+        -Player controlled rocket
+    
+    10: 
+        -Display time remaining
+        -New animated sprite of ships
+        -Paralax scrolling
+
+    20: 
+        -New Smaller spaceships worth more
+        -Alternating 2 players
+        -
+    
+    Total: 
+        80
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
